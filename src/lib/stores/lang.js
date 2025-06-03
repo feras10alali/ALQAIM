daeec6fa9486a177f14a2057bc39825c65c49e6a
feras <feras10personal@gmail.com>
2025-06-03 14:13:49 +0300

    format

--- src/lib/stores/lang.js
+++
@@ -1,9 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const lang = writable(localStorage.getItem('lang') || 'en');
-
-lang.subscribe((value) => {
-    localStorage.setItem('lang', value);
-    document.documentElement.lang = value;
-    document.documentElement.dir = value === 'ar' ? 'rtl' : 'ltr';
-});
